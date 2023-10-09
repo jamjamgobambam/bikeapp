e797996987c340ec127802152fad24559a7bf1dc
Erik <erik@code.org>
2023-10-09 15:24:05 -0500

    erik-added time class

---
+++ src/main/java/com/milliondollawinners/Time.java
@@ -0,0 +1,45 @@
+package com.milliondollawinners;
+
+public class Time {
+    //instance variables
+    int hour;
+    int minute;
+
+    // constructors
+    public Time() {
+        this(0, 0);
+    }
+
+    public Time(int hour, int minute) {
+        this.hour = hour;
+        this.minute = minute;
+    }
+    
+    //methods
+    public String toString() {
+        return hour + ":" + minute;
+    }
+
+    //accessors
+    public int getHour() {
+        return hour;
+    }
+    
+    public int getMinute() {
+        return minute;
+    }
+
+    //mutators
+    public void setHour(int hour) {
+        if (hour >= 0 && hour <= 23) {
+            this.hour = hour;
+        }
+    }
+
+    public void setMinute(int minute) {
+        if (minute >= 0 && minute <= 59) {
+            this.minute = minute;
+        }
+    }
+
+}
