41155bc0bbed2214dba6af1c480a488a4f7d5a27
Erik <erik@code.org>
2023-10-16 16:20:38 -0500

    create weather class

--- src/main/java/com/milliondollawinners/App.java
+++ src/main/java/com/milliondollawinners/App.java
@@ -2,29 +2,12 @@ package com.milliondollawinners;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Weather weather1 = new Weather();
+        Weather weather2 = new Weather(140, "cloudy");
 
-        Date birthday = new Date();
-        System.out.println(birthday.getMonth());
+        System.out.println(weather1);
+        System.out.println(weather2);
 
-        Date kensBirthday = new Date(11, 11, 1986);
-        System.out.println(kensBirthday.getMonth());
-
-        System.out.println(kensBirthday);
-
-        kensBirthday.setMonth(2);
-        System.out.println(kensBirthday);
-        kensBirthday.setMonth(14);  // should not change month
-        System.out.println(kensBirthday);
-
-
-        Location austin = new Location();
-        System.out.println(austin);
-
-        Location snohomish = new Location(47.919167, -122.09111, "Snohomish");
-        System.out.println(snohomish.getLat());
-        System.out.println(snohomish.getLon());
-
-        System.out.println(snohomish);
+        weather1.setTemp();
     }
 }
