298787520020f0ec6c10eca37470dbd96570d61b
amy-b <amy.berkhoudt@gmail.com>
2023-10-16 14:12:35 -0700

    Create a new class called Weather.java

--- src/main/java/com/milliondollawinners/App.java
+++ src/main/java/com/milliondollawinners/App.java
@@ -1,30 +1,18 @@
 package com.milliondollawinners;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-
-        Date birthday = new Date();
-        System.out.println(birthday.getMonth());
-
-        Date kensBirthday = new Date(11, 11, 1986);
-        System.out.println(kensBirthday.getMonth());
-
-        System.out.println(kensBirthday);
-
-        kensBirthday.setMonth(2);
-        System.out.println(kensBirthday);
-
-        Location austin = new Location();
-        System.out.println(austin);
-
-        Location snohomish = new Location(47.919167, -122.09111, "Snohomish");
-        System.out.println(snohomish.getLat());
-        System.out.println(snohomish.getLon());
-
-        System.out.println(snohomish);
+        Scanner in = new Scanner(System.in);
 
         Weather temp = new Weather();
-        System.out.println(temp);
+        System.out.println(temp.getTemp());
+
+        while (!= temp.getGoodTemp()) {
+            System.out.print("Please enter a new temperature: ");
+            temp.setTemp(in.nextInt());
+            System.out.println(temp.getTemp());
+        }
     }
 }
--- src/main/java/com/milliondollawinners/Weather.java
+++ src/main/java/com/milliondollawinners/Weather.java
@@ -4,6 +4,7 @@ public class Weather {
     // instance variables
     private int temp;
     private String condition;
+    private boolean goodTemp;
 
     // constructors
     public Weather() {
@@ -13,12 +14,37 @@ public class Weather {
     // parameterized constructor
     public Weather(int temp, String condition) {
         this.temp = temp;
+        /*
+         * if (temp >= -40 && temp <= 120) {
+         * this.goodTemp = true;
+         * } else
+         * this.goodTemp = false;
+         */
         this.condition = condition;
+        this.goodTemp = false;
     }
 
     // methods
     public String toString() {
-        return temp + " degrees and " + condition;
+        return temp + " degrees and " + condition + " today!";
+    }
+
+    // accessors (or getters)
+    public int getTemp() {
+        return temp;
+    }
+
+    public int getGoodTemp(){
+        return 
+    }
+
+    // mutators (or setters)
+    public void setTemp(int temp) {
+        if (temp >= -40 && temp <= 120) {
+            this.temp = temp;
+        } else {
+            System.out.println("This temperature is too extreme! Do not ride!");
+        }
     }
 
 }
