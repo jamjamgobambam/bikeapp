119adba7195c8a9ce2c724452a54c10ada85d1c4
micahkennedy <mkennedy@edc.org>
2023-10-16 16:48:14 -0400

    added challenges class

---
+++ src/main/java/com/milliondollawinners/Challenges.java
@@ -0,0 +1,23 @@
+package com.exmaple;
+
+public class Challenges {
+
+    // instance variables
+    private String challengeDescription;
+
+    // constructors 
+    public Challenges() {
+        challengeDescription = "No description for this challenge.";
+    }
+
+    public Challenges(String challengeDescription) {
+        this.challengeDescription = challengeDescription;
+    }
+
+    // methods
+    // accessor methods
+
+    public String getChallengeDescription() {
+        return challengeDescription;
+    }
+}
