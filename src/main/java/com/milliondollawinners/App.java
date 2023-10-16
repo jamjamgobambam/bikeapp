be07bfbe9f6c4f0cd5f22422de86660d9a87fca8
Ken Akiha <95503833+kakiha11@users.noreply.github.com>
2023-10-16 14:40:49 -0700

    Test with scanner and additions to event class

--- src/main/java/com/milliondollawinners/App.java
+++ src/main/java/com/milliondollawinners/App.java
@@ -1,30 +1,65 @@
 package com.milliondollawinners;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        // System.out.println("Hello world");
 
-        Date birthday = new Date();
-        System.out.println(birthday.getMonth());
+        // Date birthday = new Date();
+        // System.out.println(birthday.getMonth());
 
-        Date kensBirthday = new Date(11, 11, 1986);
-        System.out.println(kensBirthday.getMonth());
+        // Date kensBirthday = new Date(11, 11, 1986);
+        // System.out.println(kensBirthday.getMonth());
 
-        System.out.println(kensBirthday);
+        // System.out.println(kensBirthday);
 
-        kensBirthday.setMonth(2);
-        System.out.println(kensBirthday);
-        kensBirthday.setMonth(14);  // should not change month
-        System.out.println(kensBirthday);
+        // kensBirthday.setMonth(2);
+        // System.out.println(kensBirthday);
+        // kensBirthday.setMonth(14);  // should not change month
+        // System.out.println(kensBirthday);
 
 
-        Location austin = new Location();
-        System.out.println(austin);
+        // Location austin = new Location();
+        // System.out.println(austin);
 
-        Location snohomish = new Location(47.919167, -122.09111, "Snohomish");
-        System.out.println(snohomish.getLat());
-        System.out.println(snohomish.getLon());
+        // Location snohomish = new Location(47.919167, -122.09111, "Snohomish");
+        // System.out.println(snohomish.getLat());
+        // System.out.println(snohomish.getLon());
 
-        System.out.println(snohomish);
+        // System.out.println(snohomish);
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the month of your event (1-12): ");
+        int month = input.nextInt();
+        input.nextLine();
+
+        System.out.print("Enter the day of your event (1-31): ");
+        int day = input.nextInt();
+        input.nextLine();
+
+        System.out.print("Enter the year of your event (YYYY): ");
+        int year = input.nextInt();
+        input.nextLine();
+
+
+        System.out.print("What time (hour) does your event start (0-23): ");
+        int hour = input.nextInt();
+        input.nextLine();
+
+        System.out.print("What time (minute) does your event start (0-59): ");
+        int minute = input.nextInt();
+        input.nextLine();
+
+        System.out.print("Enter the name of the event: ");
+        String name = input.nextLine();
+
+        Event test = new Event(name, month, day, year, hour, minute);
+        System.out.println(test);
+
+
+
+
+
+
     }
 }
--- src/main/java/com/milliondollawinners/Event.java
+++ src/main/java/com/milliondollawinners/Event.java
@@ -1,8 +1,10 @@
 package com.milliondollawinners;
+import java.util.ArrayList;
 
 public class Event {
     private String name;
     private Date date;
+    private Time time;
     private Location location;
     private ArrayList<String> riders;
     private String[] organizers;
@@ -13,9 +15,10 @@ public class Event {
     }
 
     // parameterized constructor
-    public Event(String name, int month, int day, int year) {
+    public Event(String name, int month, int day, int year, int hour, int minute) {
         this.name = name;
         this.date = new Date(month, day, year);
+        this.time = new Time(hour, minute);
     }
 
     // methods
